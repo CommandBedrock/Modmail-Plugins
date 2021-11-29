bb7b902a6564c55c9687a8d11498fe92eac558c6
CommandBedrock <94737543+CommandBedrock@users.noreply.github.com>
2021-11-29 13:54:30 +0800

    Update nitroboost.py

--- nitroboost/nitroboost.py
+++ nitroboost/nitroboost.py
@@ -4,7 +4,6 @@ class BoostPlugin(commands.Cog):
     def __init__ (self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
     async def on_message(self, message):
 
         print(message.type)
